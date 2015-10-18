be1843e8a534fc622340023d39aa81ce321b138b
maloz <newsletteruht@gmail.com>
2015-10-18 15:58:40 +0200

    add avatar

--- gulpfile.js/config/stylus.js
+++ gulpfile.js/config/stylus.js
@@ -3,15 +3,21 @@ var config = require('./');
 module.exports = {
 	watch: [
 	config.sourceDirectory + '/core/css/**/*.styl',
-	config.sourceDirectory + '/module/**/*.styl'],
+	config.sourceDirectory + '/module/**/*.styl',
+	config.sourceDirectory + '/page/**/*.styl'
+	],
 	src: [
 	config.sourceDirectory + '/core/css/**/style.styl',
 	config.sourceDirectory + '/core/css/vendor/*.css'
+	],
+	srcpage: [
+	config.sourceDirectory + '/page/**/*.styl'
 	],
 	srcadmin: [
 	config.sourceDirectory + '/core/css/**/admin.styl',
 	config.sourceDirectory + '/core/css/vendor/flexislider.css'
 	// '!' + config.sourceDirectory + '/core/css/vendor/flexislider.css'
 	],
-	dest: config.publicDirectory + '/css'
+	dest: config.publicDirectory + '/css',
+	destpage: config.publicDirectory + '/css/page'
 };
